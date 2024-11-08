8e8ceebc8fda0cda1b4de3b4cc7cd02f3be3ad74
Kirill <verikirill@mail.ru>
2024-11-09 00:54:36 +0300

    pupupu

--- myproject2/loginsys/urls.py
+++ myproject2/loginsys/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),  # Используем встроенное представление для выхода
     path('enter-code/', views.enter_code, name='enter_code'),
     path('profile/', views.profile, name='profile'),
 ]
