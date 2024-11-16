a2f74e7c4acae020519e486bb92ce4401140f335
Chipinkoskin <118463466+verikirill@users.noreply.github.com>
2024-11-16 04:18:45 +0300

    Delete myproject2 directory

--- myproject2/myapp2/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ContactListCreate
-from .views import yandex_handler
-
-urlpatterns = [
-    path('contacts/', ContactListCreate.as_view(), name='contact-list-create'),
-    path('yandex_alice/', yandex_handler, name='yandex_handler')
-]
