6fb91d8e5fecc96316cab6da3c789b1abe0aec59
Chipinkoskin <118463466+verikirill@users.noreply.github.com>
2024-11-21 21:44:42 +0300

    Delete myproject2 directory

--- myproject2/myproject2/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-# маршруты URL для всего проекта
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('myapp2.urls')),
-    path('', include('loginsys.urls')),
-]
