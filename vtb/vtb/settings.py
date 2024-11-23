741ab12b59bea07e32119a3ff3b3c6afa317abf5
Chipinkoskin <118463466+verikirill@users.noreply.github.com>
2024-11-23 15:29:20 +0300

    Update settings.py

--- vtb/vtb/settings.py
+++ vtb/vtb/settings.py
@@ -73,9 +73,13 @@ WSGI_APPLICATION = "vtb.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    ‘default’: {
+        ‘ENGINE’: ‘django.db.backends.postgresql’,
+        ‘NAME’: ‘vtb_alice’,
+        ‘USER’: ‘vtb_alice’,
+        ‘PASSWORD’: ‘superpassword2006’,
+        ‘HOST’: ‘localhost’,
+        ‘PORT’: ‘5432’,
     }
 }
 
